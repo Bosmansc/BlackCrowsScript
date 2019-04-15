5308f6c2315e05c24f0537b9f529d29d46c69adb
bosmansc <bosmansc6@gmail.com>
2019-04-15 21:46:49 +0200

    prices retracted from html

--- SkiScraping.py
+++ SkiScraping.py
@@ -1,4 +1,40 @@
 ## track the sales price of a set of skis on an online website
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
+
+# import libraries
+import  urllib.request as url
+from bs4 import BeautifulSoup
+import pandas as pd
+import datetime
+
+# specify the url
+ski_page = 'https://www.snowcountry.nl/black-crows-atris-47323.html'
+
+# query the website and return the html to the variable ‘page’
+html = url.urlopen(ski_page)
+
+# parse the html using beautiful soup and store in variable `soup`
+soup = BeautifulSoup(html, 'html.parser')
+
+# Take out the <div> of name and get its value
+price_box = soup.find('p', attrs={'class': 'special-price'})
+
+# Find the prices in the html
+prices = soup.find_all("span", class_="price")
+sales_price = prices[1].text.replace('€','').strip()
+original_price = prices[2].text.replace('€','').strip()
+
+# Set the prices in a dataframe
+ski_data = [sales_price, original_price]
+
+
+ski_df = pd.DataFrame( columns = ['sales_price', 'original_price'] )
+ski_df.loc[datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")] = ski_data
+
+
+#ski_df.loc[datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")] = ski_data
+
+
+
+
+
 
